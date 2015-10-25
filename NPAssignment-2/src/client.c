91f88092b74f7278e6ba5c10fbd9b6ef417ec19a
Sidhartha <siddhu.thota@gmail.com>
2015-10-25 14:17:40 -0400

    client code clean up and first packet FIN handling

--- NPAssignment-2/src/client.c
+++ NPAssignment-2/src/client.c
@@ -364,89 +364,6 @@ sendAcknowledgement(int sockfd, uint32_t ts, uint32_t seq, uint32_t windowSize, 
 	sendto(sockfd,(void *)&send_pload,sizeof(send_pload),0,NULL,0);
 	printf("Sent ack with seq num : %d of type %d with window size %d \n", send_pload.ack, type, send_pload.windowSize);
 }
-//
-//int getPacket(struct dg_payload *pload, struct dg_payload *data_temp_buff, int seq_num, int windowSize)
-//{
-//	int i;
-//	for(i = 0; i < windowSize; i++)
-//	{
-//		if(data_temp_buff[i].seq_number == seq_num)
-//		{
-//			pload->ack = data_temp_buff[i].ack;
-//			strcpy(pload->buff, data_temp_buff[i].buff);
-//			pload->type = data_temp_buff[i].type;
-//			pload->seq_number = data_temp_buff[i].seq_number;
-//			pload->ts = data_temp_buff[i].ts;
-//			pload->windowSize = data_temp_buff[i].windowSize;
-//			data_temp_buff[i].type = USED;
-//			return 1;
-//		}
-//	}
-//	return 0;
-//}
-//
-//int storePacket(struct dg_payload pload, struct dg_payload* data_temp_buff, int windowSize)
-//{
-//	int i;
-//	bool isPresent = false;
-//	for(i = 0; i < windowSize; i++)
-//	{
-//		if(pload.seq_number == data_temp_buff[i].seq_number)
-//		{
-//			isPresent = true;
-//			printf("Already present in buffer %d \n", data_temp_buff[i].seq_number);
-//			break;
-//		}
-//	}
-//
-//	for(i = 0; i < windowSize && !isPresent; i++)
-//	{
-//		if(data_temp_buff[i].type == USED)
-//		{
-//			data_temp_buff[i] = pload;
-//			printf("Stored buffer %d \n", data_temp_buff[i].seq_number);
-//			return 1;
-//		}
-//	}
-//
-//	return 0;
-//}
-//
-//int isNewPacketPresent(struct dg_payload* data_temp_buff, int windowSize)
-//{
-//	int i;
-//	for(i = 0; i < windowSize; i++)
-//	{
-//		if(data_temp_buff[i].type != USED)
-//		{
-//			return 1;
-//		}
-//	}
-//	return 0;
-//}
-//
-//void initializeTempBuff(struct dg_payload* data_temp_buff, int windowSize)
-//{
-//	int i;
-//	for(i = 0; i < windowSize; i++)
-//	{
-//		data_temp_buff[i].type = USED;
-//	}
-//}
-//
-//int getUsedTempBuffSize(struct dg_payload* data_temp_buff, int windowSize)
-//{
-//	int count = 0;
-//	int i;
-//	for(i = 0; i < windowSize; i++)
-//	{
-//		if(data_temp_buff[i].type != USED)
-//		{
-//			count++;
-//		}
-//	}
-//	return count;
-//}
 
 int getWindowSize(uint32_t windowSize, int temp_buff_size)
 {
@@ -507,14 +424,15 @@ bool popData()
 
 	while(front != rear)
 	{
-		//printf("Data packet seq number is %d and data is %s\n", front->seqNum, front->buff);
-		printf("Printing Data packet seq number is %d\n", front->seqNum);
-		front = front -> next;
-		filled_circular_buffer_size--; // Window size
 		if(front -> type == FIN)
 		{
 			return true;
 		}
+
+		//printf("Data packet seq number is %d and data is %s\n", front->seqNum, front->buff);
+		printf("Printing Data packet seq number is %d\n", front->seqNum);
+		filled_circular_buffer_size--; // Window size
+		front = front -> next;
 	}
 	return false;
 }
